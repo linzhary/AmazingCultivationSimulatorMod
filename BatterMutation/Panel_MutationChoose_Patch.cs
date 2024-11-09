ea8c1f89ff39cf10199119ee6a312f23c3aedd26
linzhary <magic-lamb@outlook.com>
2024-11-10 00:48:47 +0800

    优化逻辑

--- BatterMutation/Panel_MutationChoose_Patch.cs
+++ BatterMutation/Panel_MutationChoose_Patch.cs
@@ -3,6 +3,7 @@ using HarmonyLib;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Reflection;
 using System.Reflection.Emit;
 using UnityEngine;
@@ -14,13 +15,22 @@ namespace BatterMutation
     [HarmonyPatch(typeof(Panel_MutationChoose))]
     public class Panel_MutationChoose_Patch
     {
-        protected static readonly FieldInfo _field_Result = typeof(Panel_MutationChoose).GetField("Result", BindingFlags.NonPublic | BindingFlags.Instance);
-        protected static UI_MutationConfirmBtn _cardRedrawBtn;
-        protected static int _cardRedrawBtnUsedTime = 0;
-        protected static UI_MutationConfirmBtn _groupRedrawBtn;
-        protected static int _groupRedrawBtnUsedTime = 0;
+        internal static readonly FieldInfo Field_Result = typeof(Panel_MutationChoose).GetField("Result", BindingFlags.NonPublic | BindingFlags.Instance);
+        internal static readonly FieldInfo Field_Data = typeof(Panel_MutationChoose).GetField("Data", BindingFlags.NonPublic | BindingFlags.Instance);
+        internal static UI_MutationChoosePanel UIInfo;
+        internal static UI_MutationConfirmBtn CardRedrawBtn;
+        internal static UI_MutationConfirmBtn GroupRedrawBtn;
+        internal static Panel_MutationChoose Instance;
+        internal static MutationData Data;
+        internal static g_emMutationType MutationType;
+        internal static readonly Dictionary<g_emMutationType, int?> RedrawBtnUsedTimeMap = new Dictionary<g_emMutationType, int?>()
+        {
+            { g_emMutationType.None, 0 },
+            { g_emMutationType.Constant, 0 },
+            { g_emMutationType.Unstable, 0 },
+            { g_emMutationType.YinYang, 0 }
+        };
 
-        // Token: 0x04000001 RID: 1
         public static bool LimitEnabled = true;
 
         [HarmonyPrefix]
@@ -32,13 +42,14 @@ namespace BatterMutation
             ref Action<MutationSelectResult> onFillResult
             )
         {
+            Instance = __instance;
+            UIInfo = Wnd_MutationMain.Instance.UIInfo.m_n8;
             var sourceOnFillResult = onFillResult;
             onFillResult = result =>
             {
                 if (!(result is MutationRedrawResult))
                 {
-                    _cardRedrawBtnUsedTime = 0;
-                    _groupRedrawBtnUsedTime = 0;
+                    RedrawBtnUsedTimeMap[MutationType] = 0;
                 }
                 sourceOnFillResult(result);
             };
@@ -48,14 +59,14 @@ namespace BatterMutation
 
         private static void InitCardRedrawBtn(Panel_MutationChoose instance, UI_MutationChoosePanel uiInfo, Action<MutationSelectResult> onFillResult)
         {
-            _cardRedrawBtn = UI_MutationConfirmBtn.CreateInstance();
-            uiInfo.AddChild(_cardRedrawBtn);
-            _cardRedrawBtn.SetScale(uiInfo.m_Confirm.scaleX, uiInfo.m_Confirm.scaleY);
-            _cardRedrawBtn.SetPivot(uiInfo.m_Confirm.pivotX, uiInfo.m_Confirm.pivotY, uiInfo.m_Confirm.pivotAsAnchor);
-            _cardRedrawBtn.SetSize(uiInfo.m_Confirm.width, uiInfo.m_Confirm.height);
-            _cardRedrawBtn.onClick.Set(new EventCallback0(() =>
+            CardRedrawBtn = UI_MutationConfirmBtn.CreateInstance();
+            uiInfo.AddChild(CardRedrawBtn);
+            CardRedrawBtn.SetScale(uiInfo.m_Confirm.scaleX, uiInfo.m_Confirm.scaleY);
+            CardRedrawBtn.SetPivot(uiInfo.m_Confirm.pivotX, uiInfo.m_Confirm.pivotY, uiInfo.m_Confirm.pivotAsAnchor);
+            CardRedrawBtn.SetSize(uiInfo.m_Confirm.width, uiInfo.m_Confirm.height);
+            CardRedrawBtn.onClick.Set(new EventCallback0(() =>
             {
-                if (_cardRedrawBtn.grayed)
+                if (CardRedrawBtn.grayed)
                 {
                     Wnd_PublicTips.PopupToCheterSimple("你太黑啦，重抽也救不了你");
                 }
@@ -63,31 +74,31 @@ namespace BatterMutation
                 {
                     if (LimitEnabled)
                     {
-                        _cardRedrawBtnUsedTime++;
+                        RedrawBtnUsedTimeMap[MutationType]++;
                     }
                     else
                     {
-                        _cardRedrawBtnUsedTime = 0;
+                        RedrawBtnUsedTimeMap[MutationType] = 0;
                     }
                     var result = new MutationRedrawResult();
-                    _field_Result.SetValue(instance, result);
+                    Field_Result.SetValue(instance, result);
                     onFillResult?.Invoke(result);
                 }
             }));
-            _cardRedrawBtn.visible = false;
-            _cardRedrawBtn.alpha = 0f;
-            _cardRedrawBtn.grayed = false;
+            CardRedrawBtn.visible = false;
+            CardRedrawBtn.alpha = 0f;
+            CardRedrawBtn.grayed = false;
         }
         private static void InitGroupRedrawBtn(Panel_MutationChoose instance, UI_MutationChoosePanel uiInfo, Action<MutationSelectResult> onFillResult)
         {
-            _groupRedrawBtn = UI_MutationConfirmBtn.CreateInstance();
-            uiInfo.AddChild(_groupRedrawBtn);
-            _groupRedrawBtn.SetScale(uiInfo.m_ConfirmGroup.scaleX, uiInfo.m_ConfirmGroup.scaleY);
-            _groupRedrawBtn.SetPivot(uiInfo.m_ConfirmGroup.pivotX, uiInfo.m_ConfirmGroup.pivotY, uiInfo.m_ConfirmGroup.pivotAsAnchor);
-            _groupRedrawBtn.SetSize(uiInfo.m_ConfirmGroup.width, uiInfo.m_ConfirmGroup.height);
-            _groupRedrawBtn.onClick.Set(new EventCallback0(() =>
+            GroupRedrawBtn = UI_MutationConfirmBtn.CreateInstance();
+            uiInfo.AddChild(GroupRedrawBtn);
+            GroupRedrawBtn.SetScale(uiInfo.m_ConfirmGroup.scaleX, uiInfo.m_ConfirmGroup.scaleY);
+            GroupRedrawBtn.SetPivot(uiInfo.m_ConfirmGroup.pivotX, uiInfo.m_ConfirmGroup.pivotY, uiInfo.m_ConfirmGroup.pivotAsAnchor);
+            GroupRedrawBtn.SetSize(uiInfo.m_ConfirmGroup.width, uiInfo.m_ConfirmGroup.height);
+            GroupRedrawBtn.onClick.Set(new EventCallback0(() =>
             {
-                if (_groupRedrawBtn.grayed)
+                if (GroupRedrawBtn.grayed)
                 {
                     Wnd_PublicTips.PopupToCheterSimple("你太黑啦，重抽也救不了你");
                 }
@@ -95,35 +106,44 @@ namespace BatterMutation
                 {
                     if (LimitEnabled)
                     {
-                        _groupRedrawBtnUsedTime++;
+                        RedrawBtnUsedTimeMap[MutationType]++;
                     }
                     else
                     {
-                        _groupRedrawBtnUsedTime = 0;
+                        RedrawBtnUsedTimeMap[MutationType] = 0;
                     }
                     var result = new MutationRedrawResult();
-                    _field_Result.SetValue(instance, result);
+                    Field_Result.SetValue(instance, result);
                     onFillResult?.Invoke(result);
                 }
             }));
-            _groupRedrawBtn.visible = false;
-            _groupRedrawBtn.alpha = 0f;
-            _groupRedrawBtn.grayed = false;
+            GroupRedrawBtn.visible = false;
+            GroupRedrawBtn.alpha = 0f;
+            GroupRedrawBtn.grayed = false;
+        }
+
+
+        [HarmonyPostfix]
+        [HarmonyPatch("Run")]
+        public static void On_Run_Prefix(MutationData data)
+        {
+            Data = data;
+            MutationType = MutationMgr.Instance.GetMutationTypeEnumByName(data.Type);
         }
 
         [HarmonyPostfix]
         [HarmonyPatch("ResetAnimUI")]
         public static void On_ResetAnimUI_Postfix()
         {
-            if (_cardRedrawBtn != null)
+            if (CardRedrawBtn != null)
             {
-                _cardRedrawBtn.visible = false;
-                _cardRedrawBtn.alpha = 0f;
+                CardRedrawBtn.visible = false;
+                CardRedrawBtn.alpha = 0f;
             }
-            if (_groupRedrawBtn != null)
+            if (GroupRedrawBtn != null)
             {
-                _groupRedrawBtn.visible = false;
-                _groupRedrawBtn.alpha = 0f;
+                GroupRedrawBtn.visible = false;
+                GroupRedrawBtn.alpha = 0f;
             }
         }
 
@@ -131,10 +151,10 @@ namespace BatterMutation
         [HarmonyPatch("OnClickRandom")]
         public static void On_OnClickRandom_Prefix()
         {
-            if (_groupRedrawBtn != null)
+            if (GroupRedrawBtn != null)
             {
-                _groupRedrawBtn.visible = false;
-                _groupRedrawBtn.alpha = 0f;
+                GroupRedrawBtn.visible = false;
+                GroupRedrawBtn.alpha = 0f;
             }
         }
 
@@ -228,34 +248,34 @@ namespace BatterMutation
         }
         public static void ShowCardRedrawBtn()
         {
-            var UIInfo = Wnd_MutationMain.Instance.UIInfo.m_n8;
-            if (_cardRedrawBtn != null)
+            var usedTime = RedrawBtnUsedTimeMap[MutationType];
+            if (CardRedrawBtn != null)
             {
-                _cardRedrawBtn.SetXY(UIInfo.m_Confirm.x, UIInfo.m_Confirm.y + UIInfo.m_Confirm.height / 1.5f);
-                _cardRedrawBtn.text = "重抽";
+                CardRedrawBtn.SetXY(UIInfo.m_Confirm.x, UIInfo.m_Confirm.y + UIInfo.m_Confirm.height / 1.5f);
+                CardRedrawBtn.text = "重抽";
                 if (LimitEnabled)
                 {
-                    _cardRedrawBtn.text = $"重抽({3 - _cardRedrawBtnUsedTime})";
-                    _cardRedrawBtn.grayed = _cardRedrawBtnUsedTime == 3;
+                    CardRedrawBtn.text = $"重抽({3 - usedTime})";
+                    CardRedrawBtn.grayed = usedTime == 3;
                 }
-                _cardRedrawBtn.visible = true;
-                _cardRedrawBtn.alpha = 1f;
+                CardRedrawBtn.visible = true;
+                CardRedrawBtn.alpha = 1f;
             }
         }
         public static void ShowGroupRedrawBtn()
         {
-            var UIInfo = Wnd_MutationMain.Instance.UIInfo.m_n8;
-            if (_groupRedrawBtn != null)
+            var usedTime = RedrawBtnUsedTimeMap[MutationType];
+            if (GroupRedrawBtn != null)
             {
-                _groupRedrawBtn.SetXY(UIInfo.m_ConfirmGroup.x, UIInfo.m_ConfirmGroup.y + UIInfo.m_ConfirmGroup.height);
-                _groupRedrawBtn.text = "重抽";
+                GroupRedrawBtn.SetXY(UIInfo.m_ConfirmGroup.x, UIInfo.m_ConfirmGroup.y + UIInfo.m_ConfirmGroup.height);
+                GroupRedrawBtn.text = "重抽";
                 if (LimitEnabled)
                 {
-                    _groupRedrawBtn.text = $"重抽({3 - _groupRedrawBtnUsedTime})";
-                    _groupRedrawBtn.grayed = _groupRedrawBtnUsedTime == 3;
+                    GroupRedrawBtn.text = $"重抽({3 - usedTime})";
+                    GroupRedrawBtn.grayed = usedTime == 3;
                 }
-                _groupRedrawBtn.visible = true;
-                _groupRedrawBtn.alpha = 1f;
+                GroupRedrawBtn.visible = true;
+                GroupRedrawBtn.alpha = 1f;
             }
         }
     }
--- BatterMutation/Panel_MutationResult_Patch.cs
+++ BatterMutation/Panel_MutationResult_Patch.cs
@@ -1,4 +1,6 @@
 ﻿using HarmonyLib;
+using System.Collections;
+using System.Collections.Generic;
 using System.Reflection;
 using XiaWorld;
 
@@ -7,17 +9,20 @@ namespace BatterMutation
     [HarmonyPatch(typeof(Panel_MutationResult))]
     public class Panel_MutationResult_Patch
     {
-        protected static readonly MethodInfo _method_OpenPanel = typeof(Wnd_MutationMain).GetMethod("OpenPanel", BindingFlags.NonPublic | BindingFlags.Instance);
+        internal static readonly MethodInfo Method_OpenPanel = typeof(Wnd_MutationMain).GetMethod("OpenPanel", BindingFlags.NonPublic | BindingFlags.Instance);
+        internal static readonly FieldInfo Field_SelectResults = typeof(Wnd_MutationMain).GetField("SelectResults", BindingFlags.NonPublic | BindingFlags.Instance);
+        
         [HarmonyPrefix]
         [HarmonyPatch("Run")]
         public static bool On_Run_Prefix(MutationData data, object[] objs)
         {
             if (data == null) return true;
-            var realResult = objs.SafeGet(0) as MutationRedrawResult;
-            if (realResult != null)
+            if (objs.SafeGet(0) as MutationRedrawResult != null)
             {
-                _method_OpenPanel.Invoke(Wnd_MutationMain.Instance, new object[] { g_emMutationPanel.Choose });
-                realResult.Data = data;
+                Method_OpenPanel.Invoke(Wnd_MutationMain.Instance, new object[] { g_emMutationPanel.Choose });
+                var selectResults = Field_SelectResults.GetValue(Wnd_MutationMain.Instance) as List<MutationSelectResult>;
+                selectResults.RemoveAt(selectResults.Count - 1);
+                Panel_MutationChoose_Patch.Instance.Run(data);
                 return false;
             }
             return true;
