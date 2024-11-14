2c417fd41ce25cfcdc612f6232d5d0a24e1fac29
linzhary <magic-lamb@outlook.com>
2024-11-14 09:10:17 +0800

    Add GMathUtil_Patch

--- BatterMutation/BatterMutation.cs
+++ BatterMutation/BatterMutation.cs
@@ -13,12 +13,14 @@ namespace BatterMutation
         public static void OnLoad()
         {
             Configuration.AddCheckBox("BatterMutation", "Limit", "限制次数", true);
+            Configuration.AddCheckBox("BatterMutation", "RealRandom", "完全随机", false);
             Configuration.Subscribe(new EventCallback0(HandleConfig));
         }
 
         private static void HandleConfig()
         {
             Panel_MutationChoose_Patch.LimitEnabled = Configuration.GetCheckBox("BatterMutation", "Limit");
+            GMathUtil_Patch.Enabled = Configuration.GetCheckBox("BatterMutation", "RealRandom");
         }
     }
 }
